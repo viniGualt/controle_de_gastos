3a4b24ac2e1c164f4457b58197974a82b985d6ed
ricksantana19 <hsantana639@gmail.com>
2024-11-25 22:08:03 -0300

    atualização categorias

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $usuario = 'root';
 $senha = '';
-$banco = 'categorias';
+$banco = 'contabilidade';
 
 $conn = mysqli_connect($host, $usuario, $senha, $banco) or die('Não foi possível conectar-se ao banco');
 
